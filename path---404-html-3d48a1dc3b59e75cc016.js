1203b1f76c0c41d67531bde1e53f73e58cb3cda5
Maximiliano Valencia <mavamo135@gmail.com>
2019-04-05 14:39:44 -0600

    Updates

--- path---404-html-3d48a1dc3b59e75cc016.js
+++ path---404-html-3d48a1dc3b59e75cc016.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xa2868bfb69fc],{544:function(t,a){t.exports={data:{site:{siteMetadata:{title:"MyBlog"}}},pathContext:{}}}});
+webpackJsonp([0xa2868bfb69fc],{376:function(t,a){t.exports={data:{site:{siteMetadata:{title:"MyBlog"}}},pathContext:{}}}});
 //# sourceMappingURL=path---404-html-3d48a1dc3b59e75cc016.js.map
